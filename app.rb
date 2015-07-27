622d943ed8fb2902b8335c30c02d35659bfba0f2
GrimmOutlook <grimmoutlook87@gmail.com>
2015-07-27 15:56:13 -0400

    Added user authentication to upload images.

--- app.rb
+++ app.rb
@@ -11,9 +11,24 @@ class App < Sinatra::Base
   end
 
   post "/images" do
+    protected!
     @image = Image.new params[:image]
     @image.save
     redirect "/"
   end
+
+  helpers do
+    def protected!
+      return if authorized?
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, "Not Authorized\n"
+    end
+
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ["Dave", "adminwordofpass"]
+    end
+  end
+
 
 end
